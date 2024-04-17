26c59a3fdfb345937813a5ff60328551d95a3e97
baheuddeen <muhammad.baheuddeen@gmail.com>
2024-04-17 21:11:25 +0200

    add trust proxy

--- src/server.ts
+++ src/server.ts
@@ -7,6 +7,8 @@ app.use('/', router);
 
 const PORT = process.env.PORT || 3000;
 
+app.set('trust proxy', true);
+
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
     }
